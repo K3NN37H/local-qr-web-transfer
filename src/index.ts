b41428270f75c04a086adef997b3e97d54955801
Ken <kjtchan@gmail.com>
2021-11-22 18:53:19 -0500

    Handle all paths, show qr code to dl page

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,43 @@
-import Koa, { Response } from 'koa'
+import Koa from 'koa'
 import send from 'koa-send'
+// import send from 'koa-sendfile'
 import { cwd, argv, exit } from 'process'
-import { basename, join } from 'path'
+import { basename, join, isAbsolute, dirname } from 'path'
+import { networkInterfaces } from 'os'
+import Qrcode from 'qrcode'
+import open from 'open'
+
 const app = new Koa();
 
-if (argv.length < 3) exit()
+if (argv.length < 3) exit(1)
+console.debug(networkInterfaces())
 
-// response
+const ip = Object.values(networkInterfaces()).flat().find(network =>
+  network?.family === 'IPv4' && network.address.startsWith('192.168')
+)?.address
+console.log(ip)
+
+const sendRoot = isAbsolute(argv[2]) ? dirname(argv[2]) : cwd()
+const filename = basename(join(sendRoot, argv[2]))
+
 app.use(async ctx => {
+  console.log(ctx.method)
   if (ctx.path === '/download') {
-    ctx.set('Content-Disposition', `attachment; filename="${basename(join(cwd(), argv[2]))}"`)
-    await send(ctx, argv[2], { root: cwd() })
+    ctx.set('Content-Disposition', `attachment; filename="${filename}"`)
+    // await send(ctx, join(sendRoot, filename))
+    await send(ctx, filename, { root: sendRoot })
+    console.debug(ctx)
   } else {
-    ctx.body = 'Hello Koa';
+    const qrdata = await Qrcode.toDataURL(`http://${ip}:3000/download`)
+    ctx.body = `<img src="${qrdata}" />`;
   }
 });
 
 app.listen(3000);
+
+open(`http://localhost:3000`)
+
+console.log('Auto shutdown in 60 seconds...')
+setTimeout(() => {
+  exit(0)
+}, 60000)
